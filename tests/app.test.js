b0148268a3de805c66f8510c54f34857b4bcbb26
Diksa <madediksapitra@gmail.com>
2024-09-16 19:16:13 +0700

    tes jest 2

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,5 +1,10 @@
 const request = require('supertest');
-const app = require('../src/app');
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
 
 describe('GET /', () => {
   it('should return Hello World', async () => {
