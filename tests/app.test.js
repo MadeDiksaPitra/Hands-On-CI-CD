6849d5a9930057eb266682e8200ba437faf6ead7
Diksa <madediksapitra@gmail.com>
2024-09-16 20:57:40 +0700

    rollback

--- tests/app.test.js
+++ tests/app.test.js
@@ -7,7 +7,7 @@ app.get('/', (req, res) => {
 });
 
 describe('GET /', () => {
-  it('should return Hello Worldd', async () => {
+  it('should return Hello World', async () => {
     const res = await request(app).get('/');
     expect(res.text).toBe('Hello World!');
     expect(res.statusCode).toBe(200);
